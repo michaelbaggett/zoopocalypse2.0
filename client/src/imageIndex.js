f264bde00ff7d90bcc9a697c88f5cc27711a94ea
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2019-01-28 12:02:57 -0600

    updated image index w new animals. ALl working except for llama

--- client/src/imageIndex.js
+++ client/src/imageIndex.js
@@ -3,12 +3,28 @@ import Fox from "./images/fox.png";
 import Bear from "./images/bear.png";
 import Lion from "./images/lion.png";
 import Tiger from "./images/tiger.png";
-import Rhino from "./images/rino.png";
+import Rhino from "./images/rhino.png";
 import Walrus from "./images/walrus.png";
-import Alpaca from "./images/llama.png";
 import Penguin from "./images/penguin.png";
 import Giraffe from "./images/giraffe.png";
 import Elephant from "./images/elephant.png";
+import Ant from "./images/ant.png";
+import Yak from "./images/yak.png";
+import Owl from "./images/owl.png";
+import Toad from "./images/toad.png";
+import Crab from "./images/crab.png";
+import Bull from "./images/bull.png";
+import Panda from "./images/panda.png";
+import Llama from "./images/llama.png";
+import Sloth from "./images/sloth.png";
+import Snake from "./images/snake.png";
+import Monkey from "./images/monkey.png";
+import Jaguar from "./images/jaguar.png";
+import Toucan from "./images/toucan.png";
+import Beaver from "./images/beaver.png";
+import Gorilla from "./images/gorilla.png";
+import Cheetah from "./images/cheetah.png";
+
 export default {
   Bat,
   Fox,
@@ -17,8 +33,23 @@ export default {
   Tiger,
   Rhino,
   Walrus,
-  Alpaca,
   Penguin,
   Giraffe,
-  Elephant
+  Elephant,
+  Ant,
+  Yak,
+  Owl,
+  Toad,
+  Crab,
+  Bull,
+  Panda,
+  Llama,
+  Sloth,
+  Snake,
+  Monkey,
+  Jaguar,
+  Toucan,
+  Beaver,
+  Gorilla,
+  Cheetah
 };
